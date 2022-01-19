8cc93174d9ac04fb47d87198e2bf34055486c717
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-01-19 12:57:38 -0600

    Create repo and add code to check if browser supports MIDI

---
+++ js/main.js
@@ -0,0 +1,17 @@
+if (navigator.requestMIDIAccess) {
+  navigator.requestMIDIAccess({
+    sysex: false  // this defaults to false
+  }).then(onMIDISuccess, onMIDIFailure);
+} else {
+  alert("No MIDI support in your browser");
+}
+
+function onMIDISuccess(midiAccess) {
+  // when we get a successful response, run this code
+  console.log('MIDI Access Object', midiAccess);
+}
+
+function onMIDIFailure(e) {
+  // when we get a failed response, run this code
+  console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+}
