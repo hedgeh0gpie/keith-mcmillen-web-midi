768dbdbbb92d09a2341106fe660f67c039f16f2f
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-01-29 17:55:42 -0600

    Add C4-B4 MIDI note to pitch conversion

--- js/main.js
+++ js/main.js
@@ -34,18 +34,14 @@ function onMIDIMessage(event) {
   // data = message.data  // This gives us our [command/channel, note, velocity] data
   // console.log('MIDI data', data); // MIDI data [144, 63, 73]
 
-    data = event.data,
-    cmd = data[0] >> 4,
-    channel = data[0] & 0xf,
-    type = data[0] & 0xf0,  // Channel agnostic message type
-    note = data[1],
-    velocity = data[2];
-    // With pressure and tilt off
-    // Note off: 128, cmd: 8
-    // Note on: 144, cmd: 9
-    // Pressure/tilt on
-    // Pressure: 176, cmd: 11
-    // Bend: 224, cmd: 14
+  data = event.data, cmd = data[0] >> 4, channel = data[0] & 0xf, type = data[0] & 0xf0,  // Channel agnostic message type
+    note = data[1], velocity = data[2];
+  // With pressure and tilt off
+  // Note off: 128, cmd: 8
+  // Note on: 144, cmd: 9
+  // Pressure/tilt on
+  // Pressure: 176, cmd: 11
+  // Bend: 224, cmd: 14
 
   // Filter out the MiniNova mystery persistent MIDI messages. I believe it must be the MIDI clock??
   if (channel !== 8 && cmd !== 15 && type !== 240) {
@@ -73,9 +69,9 @@ function onMIDIMessage(event) {
   }
 
   function player(note, velocity) {
-    }
+  }
 
-    // Utility Functions
+  // Utility Functions
   function logger(container, label, data) {
     messages = label + " [channel: " + (data[0] & 0xf) + ", cmd: " + (data[0] >> 4) + ", type: " + (data[0] & 0xf0) + " , MIDI note: " + data[1] + ", note: " + midiNoteToPitch(data) + ", velocity: " + data[2] + "]";
     console.log(messages);
@@ -84,13 +80,46 @@ function onMIDIMessage(event) {
   function midiNoteToPitch(data) {
     let midiNote = data[1];
     let pitch;
-    switch(midiNote) {
+    switch (midiNote) {
       case 60:
-        pitch = "C4 (middle C)"
+        pitch = "C4 (middle C)";
+        break;
+      case 61:
+        pitch = "C#4/Db4";
+        break;
+      case 62:
+        pitch = "D4";
+        break;
+      case 63:
+        pitch = "D#4/Eb4";
+        break;
+      case 64:
+        pitch = "E4";
+        break;
+      case 65:
+        pitch = "F4";
+        break;
+      case 66:
+        pitch = "F#4/Gb4";
+        break;
+      case 67:
+        pitch = "G4";
+        break;
+      case 68:
+        pitch = "G#4/Ab4";
+        break;
+      case 69:
+        pitch = "A4";
+        break;
+      case 70:
+        pitch = "A#4/Bb4";
+        break;
+      case 71:
+        pitch = "B4";
+        break;
     }
     return pitch;
   }
-
 
 
 }
